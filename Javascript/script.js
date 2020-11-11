3eb35fa422b24d6c16a04cf55de20bca43b565eb
dedane <evanskeema@gmail.com>
2020-11-11 20:19:58 +0300

    'carousel'

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,16 @@
 $(document).on('click', '.dropdown-menu', function (e) {
   e.stopPropagation();
 });
+$('.slider-one')
+.not(".slick-initialized")
+.slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow: ".site-slider .slider-btn .prev",
+  nextArrow: ".site-slider .slider-btn .next",
+});
