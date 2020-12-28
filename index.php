841a9b6d4242d491d73f60cad9e35cf44de5b8e8
dedane <evanskeema@gmail.com>
2020-12-28 05:54:13 +0300

    add footer

--- index.php
+++ index.php
@@ -6,19 +6,17 @@
     include('Template/banner.php');
     /* Include banner area */
 
-?>
-<?php 
-  include('Template/Logos.php');
-?>
-<?php
+    include('Template/Logos.php');
+
     include('Template/footer.php');
-?>
-<?php
+
     include('Template/mainCarousel.php');
-?>
-<?php
+
     include('Template/minicarousel.php');
-?>
-<?php
+
     include('Template/subscription.php');
+?>
+
+<?php
+    include('Template/Footer.php');
 ?>
